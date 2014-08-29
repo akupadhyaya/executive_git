e3624dd77d7ae8c106c8126912fbcf7bbe98c64b
akupadhyaya <ashishupadhyaya4@gmail.com>
2014-08-29 18:45:10 +0530

    About us title change
    About us title tag changed

--- about-us.php
+++ about-us.php
@@ -1,37 +1,74 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" dir="ltr" lang="en">
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-<title>Printfastonline | About us</title>
-<meta name="keywords" content="" />
-<meta name="description" content="" />
-<meta http-equiv="imagetoolbar" content="no" />
-<meta name="author" content="" />
-<meta name="generator" content="" />
-<?php include('includes/html-header.php'); ?>
-</head>
-<body id="indexHomeBody">
-<!-- Header Section Starts --> <?php include('includes/tpl_header.php'); ?><!-- Header Section Ends -->
-<div id="mainWrapper"> <!------- Start mainWrapper here -------------->
-<div class="main"> <!------- Start main here -------------->
-<h2>Why use Printfastonline.co.uk</h2>  
-<table width="100%" class="abt-wrp" border="0" cellspacing="0" cellpadding="0">
-  <tr>
-  
-    <td width="40%"><img src="images/about-pic.jpg" /></td>
-    
-    <td width="55%"><p><strong>Lorem ipsum</strong> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-
-<p><strong>Lorem ipsum</strong> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-
-<p><strong>Lorem ipsum</strong> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-
-<p align="right" style="font-size:14px;"> &minus; Lorem ipsum dolor sit amet</p>
-</td>
-  </tr>
-</table>
-</div> <!------- End main here -------------->
-</div> <!------- End mainWrapper here -------------->
-<!-- Footer Section Starts --> <?php include('includes/tpl_footer.php'); ?><!-- Footer Section Ends --> 
-</body>
-</html>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml" dir="ltr" lang="en">
+
+<head>
+
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+
+<title>Printfastonline AK | About us</title>
+
+<meta name="keywords" content="" />
+
+<meta name="description" content="" />
+
+<meta http-equiv="imagetoolbar" content="no" />
+
+<meta name="author" content="" />
+
+<meta name="generator" content="" />
+
+<?php include('includes/html-header.php'); ?>
+
+</head>
+
+<body id="indexHomeBody">
+
+<!-- Header Section Starts --> <?php include('includes/tpl_header.php'); ?><!-- Header Section Ends -->
+
+<div id="mainWrapper"> <!------- Start mainWrapper here -------------->
+
+<div class="main"> <!------- Start main here -------------->
+
+<h2>Why use Printfastonline.co.uk</h2>  
+
+<table width="100%" class="abt-wrp" border="0" cellspacing="0" cellpadding="0">
+
+  <tr>
+
+  
+
+    <td width="40%"><img src="images/about-pic.jpg" /></td>
+
+    
+
+    <td width="55%"><p><strong>Lorem ipsum</strong> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
+
+
+<p><strong>Lorem ipsum</strong> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
+
+
+<p><strong>Lorem ipsum</strong> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
+
+
+<p align="right" style="font-size:14px;"> &minus; Lorem ipsum dolor sit amet</p>
+
+</td>
+
+  </tr>
+
+</table>
+
+</div> <!------- End main here -------------->
+
+</div> <!------- End mainWrapper here -------------->
+
+<!-- Footer Section Starts --> <?php include('includes/tpl_footer.php'); ?><!-- Footer Section Ends --> 
+
+</body>
+
+</html>
+
